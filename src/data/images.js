f7c613cd821c0e127145cd37bf5f8217f53a032e
DAVE KRISH <2022002629.gcet@cvmu.edu.in>
2025-05-15 13:40:01 +0530

    Initial commit

---
+++ src/data/images.js
@@ -0,0 +1,103 @@
+const images = [
+  {
+    image: '/img/img1.webp'
+  },
+  {
+    image: '/img/img2.webp'
+  },
+  {
+    image: '/img/img3.webp'
+  },
+  {
+    image: '/img/img4.webp'
+  },
+  {
+    image: '/img/img5.webp'
+  },
+  {
+    image: '/img/img6.webp'
+  },
+  {
+    image: '/img/img7.webp'
+  },
+  {
+    image: '/img/img8.webp'
+  },
+  {
+    image: '/img/img9.webp'
+  },
+  {
+    image: '/img/img10.webp'
+  },
+  {
+    image: '/img/img11.webp'
+  },
+  {
+    image: '/img/img12.webp'
+  },
+  {
+    image: '/img/img13.webp'
+  },
+  {
+    image: '/img/img14.webp'
+  },
+  {
+    image: '/img/img15.webp'
+  },
+  {
+    image: '/img/img16.webp'
+  },
+  {
+    image: '/img/img17.webp'
+  },
+  {
+    image: '/img/img18.webp'
+  },
+  {
+    image: '/img/img19.webp'
+  },
+  {
+    image: '/img/img20.webp'
+  },
+  {
+    image: '/img/img21.webp'
+  },
+  {
+    image: '/img/img22.webp'
+  },
+  {
+    image: '/img/img23.webp'
+  },
+  {
+    image: '/img/img24.webp'
+  },
+  {
+    image: '/img/img25.webp'
+  },
+  {
+    image: '/img/img26.webp'
+  },
+  {
+    image: '/img/img27.webp'
+  },
+  {
+    image: '/img/img28.webp'
+  },
+  {
+    image: '/img/img29.webp'
+  },
+  {
+    image: '/img/img30.webp'
+  },
+  {
+    image: '/img/img31.webp'
+  },
+  {
+    image: '/img/img32.webp'
+  },
+  {
+    image: '/img/img33.webp'
+  }
+]
+
+export default images
